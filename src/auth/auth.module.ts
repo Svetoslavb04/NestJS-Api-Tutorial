de83fa52b786f57418362be23dfe3a07545f623b
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-01 17:07:14 +0300

    feat: add auth service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Module({
-  providers: [],
+  providers: [AuthService],
   controllers: [AuthController],
 })
 export class AuthModule {}
