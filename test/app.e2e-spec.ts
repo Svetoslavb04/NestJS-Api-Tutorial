adc4be832034e551866f51bd75d494528e6bb4ef
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-02 18:07:47 +0300

    feat: define tests structure

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,6 +3,8 @@ import { AppModule } from '../src/app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { PrismaService } from '../src/prisma/prisma.service';
 
+import * as pactum from 'pactum';
+
 describe('App e2e', () => {
   let app: INestApplication;
   let prisma: PrismaService;
@@ -21,6 +23,7 @@ describe('App e2e', () => {
     );
 
     await app.init();
+    await app.listen(3333);
 
     prisma = app.get(PrismaService);
 
@@ -31,5 +34,36 @@ describe('App e2e', () => {
     await app.close();
   });
 
-  it.todo('should pass');
+  describe('Auth', () => {
+    describe('Signup', () => {
+      it.todo('should signup');
+    });
+    describe('Signin', () => {
+      it.todo('should signin');
+    });
+  });
+
+  describe('User', () => {
+    describe('Me', () => {
+      it.todo('should get me information');
+    });
+  });
+
+  describe('Bookmarks', () => {
+    describe('Create Bookmark', () => {
+      it.todo('should create bookmark');
+    });
+    describe('Get Bookmarks', () => {
+      it.todo('should get all bookmarks');
+    });
+    describe('Get Bookmark by id', () => {
+      it.todo('should get bookmark by id');
+    });
+    describe('Edit Bookmark', () => {
+      it.todo('should edit bookmark');
+    });
+    describe('Delete Bookmark', () => {
+      it.todo('should delete bookmark by id');
+    });
+  });
 });
