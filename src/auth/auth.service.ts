ff3a8370332dcd759acaa8684bd7f2bad547ee71
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-02 16:02:39 +0300

    feat: injet JwtService inside auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,16 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { AuthDto } from './dto';
+
+import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable({})
 export class AuthService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(
+    private prismaService: PrismaService,
+    private jwtService: JwtService,
+  ) {}
 
   async signin(dto: AuthDto) {
     const user = await this.prismaService.user.findUnique({
