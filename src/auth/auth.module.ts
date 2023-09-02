010322e5417472634a29e8748de18e87a06f6c32
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-02 16:45:13 +0300

    feat: provide JWT auth strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategy';
 
 @Module({
   imports: [JwtModule.register({})],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
