8e9ae39c5470791f5368ae4ea0dc55c219e2a958
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-02 17:06:54 +0300

    feat: create custom user param decorator

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
+import { User } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
 
 @Controller('users')
 export class UserController {
   @UseGuards(JwtGuard)
   @Get('me')
-  getMe(@Req() req: Request) {
-    return req.user;
+  getMe(@User() user) {
+    return user;
   }
 }
